6680bf004104d3bc835c489c100c836350c51705
Viraj_Madusanka <kasunperera2296@gmail.com>
2024-02-26 10:02:13 +0530

    Initialize Customer UI

---
+++ BookingSystem/src/bookingsystem/dto/CustomerDto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookingsystem.dto;
+
+/**
+ *
+ * @author Dell
+ */
+public class CustomerDto {
+    
+}
